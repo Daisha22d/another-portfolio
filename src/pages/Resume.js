dfbe1694373f2ca30b4d05f25138d2df9fe9a625
Daisha <daishadeniz@gmail.com>
2023-08-16 16:13:41 -0400

    res adjusted

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import resumePDF from '../../src/res08.pdf';
+import '../pages/Resume.css';
 
 export default function Resume() {
   return (
     <div>
       <h1>Resume</h1>
-      <embed src={resumePDF} type="application/pdf" width="100%" height="600px" />
+      <embed src={resumePDF} type="application/pdf" width="150%" height="600px" />
     </div>
   );
 }

eeb4e74d50b8e7f42ba93ee6d5351b90a96759c4
Daisha <daishadeniz@gmail.com>
2023-08-16 16:27:21 -0400

    updated res

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,9 +4,11 @@ import '../pages/Resume.css';
 
 export default function Resume() {
   return (
-    <div>
-      <h1>Resume</h1>
-      <embed src={resumePDF} type="application/pdf" width="150%" height="600px" />
+    <div className="resume-container">
+      <h1 className="resume-title">Resume</h1>
+      <div className="pdf-container">
+        <embed src={resumePDF} type="application/pdf" width="100%" height="650px" />
+      </div>
     </div>
   );
 }

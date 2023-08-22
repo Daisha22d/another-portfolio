5b3bfac73b5bd48c03656cd10941565067aebff4
Daisha <daishadeniz@gmail.com>
2023-08-21 20:02:17 -0400

    routes changed

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,11 +9,23 @@ const Projects = () => {
             <p>Here are some of my projects:</p>
             <ul>
                 <li>
-                    <Link to="https://github.com/Daisha22d">GitHub</Link>
+                    <a href="https://chatbot-00-eac25725f44b.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                        Robo Bot - Assistant Chatbot
+                    </a>
+                    <p>
+                        An assistant bot that you can ask anything for help. Type a question and press send, and the bot will reply.
+                        Refresh the page to clear the conversation. Developed using Flask and deployed on Heroku. Utilized the OpenAI API.
+                    </p>
+                    <p>Technologies: Flask, Python, HTML, CSS, Heroku</p>
+                </li>
+                <li>
+                    <a href="https://github.com/Daisha22d/probable-guacamole" target="_blank" rel="noopener noreferrer">
+                        GitHub
+                    </a>
                 </li>
             </ul>
         </div>
-    )
+    );
 };
 
 export default Projects;

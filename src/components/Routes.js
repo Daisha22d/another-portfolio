0451729dccb26d4a7d5217efd73088c1d25ce3c2
Daisha <daishadeniz@gmail.com>
2023-08-24 15:37:37 -0400

    checked routes

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from '../pages/Home';
-import Projects from '../pages/Projects';
-import AboutMe from '../pages/About'; 
-import Resume from '../pages/Resume';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "../pages/Home";
+import Projects from "../pages/Projects";
+import AboutMe from "../pages/About";
+import Resume from "../pages/Resume";
 
 const Routes = () => {
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/projects" component={Projects} />
-      <Route path="/aboutme" component={AboutMe} />
-      <Route path="/resume" component={Resume} />
+      <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home} />
+      <Route path={`${process.env.PUBLIC_URL}/projects`} component={Projects} />
+      <Route path={`${process.env.PUBLIC_URL}/aboutme`} component={AboutMe} />
+      <Route path={`${process.env.PUBLIC_URL}/resume`} component={Resume} />
     </Switch>
   );
 };

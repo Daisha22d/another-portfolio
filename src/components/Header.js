960c1487d2fa6d48fd029b06f0b18a63a6a5ce96
Daisha <daishadeniz@gmail.com>
2023-08-24 20:41:07 -0400

    deploying

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useMediaQuery } from 'react-responsive';
 import Navbar from 'react-bootstrap/Navbar';
@@ -7,39 +7,90 @@ import "../components/Header.css";
 
 export default function Header() {
   const isMobile = useMediaQuery({ maxWidth: 767 });
+  const [menuExpanded, setMenuExpanded] = useState(false);
 
-  // Define responsive styles based on the screen size
-  const headerStyle = {
-    padding: "10px 20px", // Adjust padding
-    flexDirection: "row", // Keep it horizontal
-    alignItems: "center",
-    justifyContent: "space-between", // Distribute elements evenly
+  const toggleMenu = () => {
+    if (isMobile) {
+      setMenuExpanded(!menuExpanded);
+    }
   };
 
-  const logoStyle = {
-    width: "140px", // Keep logo width consistent
+  const closeMenu = () => {
+    if (isMobile) {
+      setMenuExpanded(false);
+    }
+  };
+
+  const headerStyle = {
+    padding: "10px 20px",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   };
 
   const navLinksStyle = {
-    display: "flex", // Ensure links are displayed in a row
+    display: "flex",
     alignItems: "center",
-    gap: "20px", // Adjust gap between links
+    gap: "20px",
   };
 
   return (
-    <Navbar collapseOnSelect expand="lg" variant="dark" className="navbar-purple" style={headerStyle}>
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      variant="dark"
+      className="navbar-purple"
+      style={headerStyle}
+      expanded={menuExpanded}
+    >
       <Navbar.Brand as={Link} to="/" style={{ display: "flex", alignItems: "center" }}>
         {isMobile ? (
           <span className="logo-text" style={{ marginLeft: "10px" }}></span>
         ) : null}
       </Navbar.Brand>
-      {isMobile && <Navbar.Toggle aria-controls="responsive-navbar-nav" />}
-      <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+      {isMobile && (
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          onClick={toggleMenu}
+        />
+      )}
+      <Navbar.Collapse
+        id="responsive-navbar-nav"
+        className="justify-content-end"
+      >
         <Nav style={navLinksStyle}>
-          <Nav.Link as={Link} to="/" style={{ fontWeight: "bold", color: "rgb(255, 255, 255)" }}>Home</Nav.Link>
-          <Nav.Link as={Link} to="/about" style={{ fontWeight: "bold", color: "rgb(255, 255, 255)" }}>About</Nav.Link>
-          <Nav.Link as={Link} to="/projects" style={{ fontWeight: "bold", color: "rgb(255, 255, 255)" }}>Projects</Nav.Link>
-          <Nav.Link as={Link} to="/resume" style={{ fontWeight: "bold", color: "rgb(255, 255, 255)" }}>Resume</Nav.Link>
+          <Nav.Link
+            as={Link}
+            to="/"
+            style={{ fontWeight: "bold", color: "rgb(255, 255, 255)" }}
+            onClick={closeMenu}
+          >
+            Home
+          </Nav.Link>
+          <Nav.Link
+            as={Link}
+            to="/about"
+            style={{ fontWeight: "bold", color: "rgb(255, 255, 255)" }}
+            onClick={closeMenu}
+          >
+            About
+          </Nav.Link>
+          <Nav.Link
+            as={Link}
+            to="/projects"
+            style={{ fontWeight: "bold", color: "rgb(255, 255, 255)" }}
+            onClick={closeMenu}
+          >
+            Projects
+          </Nav.Link>
+          <Nav.Link
+            as={Link}
+            to="/resume"
+            style={{ fontWeight: "bold", color: "rgb(255, 255, 255)" }}
+            onClick={closeMenu}
+          >
+            Resume
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
